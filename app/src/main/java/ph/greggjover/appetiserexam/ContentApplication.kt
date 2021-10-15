fcdb9d16ef4b8cb15e2df25177e070171325abae
Gregg Meyrick Jover <greggmeyrick@gmail.com>
2021-10-15 22:42:39 +0800

    Created ContentApplication and Changed Activity Toolbar Themes

---
+++ app/src/main/java/ph/greggjover/appetiserexam/ContentApplication.kt
@@ -0,0 +1,10 @@
+package ph.greggjover.appetiserexam
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application class for the entire app
+ */
+@HiltAndroidApp
+class ContentApplication : Application()
